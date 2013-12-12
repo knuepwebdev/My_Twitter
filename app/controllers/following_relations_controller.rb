262af7a9cc497a16ca7e49e4cb636f6866bdccc3
Mike <mikeknueppel@hotmail.com>
2013-12-11 18:34:40 -0800

    Display a flash message when following/unfollowing a user

--- app/controllers/following_relations_controller.rb
+++ app/controllers/following_relations_controller.rb
@@ -2,17 +2,16 @@ class FollowingRelationsController < ApplicationController
 	def create
 		user = find_user
 		current_user.follow(user)
-		redirect_to user
+		redirect_to user, notice: "You are now following #{user.username.capitalize}"
 	end
 
 	def destroy
 		user = find_user
 		current_user.unfollow(user)
-		redirect_to user
+		redirect_to user, notice: "You are no longer following #{user.username.capitalize}"
 	end
 
 	private
-
 	def find_user
 		User.find(params[:user_id])
 	end
