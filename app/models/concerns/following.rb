f5fc3c89e003c16505b6ea28faad6d92417960a5
Mike <mikeknueppel@hotmail.com>
2013-12-11 19:28:04 -0800

    Refactor User model -move methods into a Concerns module

---
+++ app/models/concerns/following.rb
@@ -0,0 +1,28 @@
+module Concerns
+	module Following
+		extend ActiveSupport::Concern
+
+		included do
+			has_many :followed_user_relations, 
+  					foreign_key: :follower_id,
+  					class_name: 'FollowingRelation'
+  		has_many :followed_users, through: :followed_user_relations
+ 			has_many :follower_relations, 
+ 						foreign_key: :followed_user_id, 
+ 						class_name: 'FollowingRelation' 
+ 			has_many :followers, through: :follower_relations
+		end
+
+		def following?(user)
+	 		followed_user_ids.include?(user.id)
+	 	end
+
+	 	def follow(user)
+	 		followed_users << user
+	 	end
+
+	 	def unfollow(user)
+	 		followed_users.delete(user)
+	 	end
+	end
+end	
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,9 @@
 class User < ActiveRecord::Base
+	include Concerns::Following
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
-  has_many :followed_user_relations, 
-  					foreign_key: :follower_id,
-  					class_name: 'FollowingRelation'
-  has_many :followed_users, through: :followed_user_relations
- 	has_many :follower_relations, 
- 						foreign_key: :followed_user_id, 
- 						class_name: 'FollowingRelation' 
- 	has_many :followers, through: :follower_relations
-
-
- 	def following?(user)
- 		followed_user_ids.include?(user.id)
- 	end
-
- 	def follow(user)
- 		followed_users << user
- 	end
-
- 	def unfollow(user)
- 		followed_users.delete(user)
- 	end
+  
 end
