4c12c2f22cdbee7e6fd2585a33079307244df419
Mike <mikeknueppel@hotmail.com>
2013-12-13 15:08:50 -0800

    Make the Dashboard the root route -add before_filter -if user is not logged in, redirect to login page -remove HomesController

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,14 @@
 class DashboardsController < ApplicationController
+	before_filter :logged_in_user
+
 	def show
 		@dashboard = Dashboard.new(current_user)
+	end
+
+	private
+	def logged_in_user
+		unless user_signed_in?
+			redirect_to new_user_session_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MyTwitter::Application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
-  root 'home#show'
+  root 'dashboards#show'
   resource :dashboard, only: [:show]
   resource :search, only: [:show]
   resources :tweets, only: [:new, :show]
