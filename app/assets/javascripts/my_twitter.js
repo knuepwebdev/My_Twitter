19583fa7a9b9a54eb5f00d3c7e5d11c48acfee64
Mike <mikeknueppel@hotmail.com>
2013-12-17 19:31:40 -0800

    Style forgot password page

--- app/assets/javascripts/my_twitter.js
+++ app/assets/javascripts/my_twitter.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	var path = window.location.pathname;
 
-	if (path == '/users/login' || path == '/users/sign_up') {
+	if (path == '/users/login' || 
+			path == '/users/sign_up' || 
+			path == '/users/password/new') {
 		$('header').css({
 			height: 110
 		});
