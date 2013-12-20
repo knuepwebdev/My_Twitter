e9d81f4d24eea2dc48142410d8c0a3a306193467
Mike <mikeknueppel@hotmail.com>
2013-12-19 17:17:33 -0800

    Remove s3 url parameter to has_attached_file

--- app/models/image_tweet.rb
+++ app/models/image_tweet.rb
@@ -2,7 +2,7 @@ class ImageTweet < ActiveRecord::Base
 	has_many :tweets, as: :content
 	has_attached_file :image, styles: {
 		thumb: '100x100>'
-	}, url: 's3.amazonaws.com/tweeeter', :storage => :s3,
+	}, :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
