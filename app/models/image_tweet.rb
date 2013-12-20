d33d01f334d185d133c154d55af64c807fb93fc7
Mike <mikeknueppel@hotmail.com>
2013-12-19 17:42:23 -0800

    Add s3_host_name

--- app/models/image_tweet.rb
+++ app/models/image_tweet.rb
@@ -1,13 +1,16 @@
 class ImageTweet < ActiveRecord::Base
 	has_many :tweets, as: :content
-	has_attached_file :image, styles: {
-		thumb: '100x100>'
-	}, :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+	has_attached_file :image, 
+										styles: {
+											thumb: '100x100>'
+										}, 
+										:storage => :s3,
+										:s3_protocol => 'https',
+									  :s3_credentials => {
+									    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+									    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+									  },
+									  :bucket => ENV['S3_BUCKET_NAME']
 
 	def index
 		self.image_file_name
